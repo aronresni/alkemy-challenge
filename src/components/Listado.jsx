0525b5bcc8b22b04def9d229dc09140683de068c
aronresni <aronresni@gmail.com>
2023-08-17 17:43:43 -0300

    upload

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -11,57 +11,48 @@ import Card from 'react-bootstrap/Card';
 const Listado = () => {
     const navigate = useNavigate();
     let token = localStorage.getItem("token");
-    
+
     const [moviesList, setMovieList] = useState([]);
-   
-   
+
+
     useEffect(() => {
-      
-        
+
+
         const endPoint = "https://api.themoviedb.org/3/discover/movie?api_key=0702186a1d710f439f893f1afa745a31&language=es-ES&page=1"; // Fixed typo
         axios.get(endPoint)
             .then(response => {
                 const apiData = response.data;
                 setMovieList(apiData.results)
-            }) 
+            })
             .catch(error => {
                 console.error("Error fetching data:", error);
             });
     }, [setMovieList]);
 
-console.log(moviesList);
 
     return (
         <>
-        {!token && navigate("/")}
-        <div className='row'>
-       {
-        moviesList.map((oneMovie,index)=>{
-            return(
-                 <div className='col-3'> <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="" />
-          <Card.Body>
-            <Card.Title>{oneMovie.tittle}</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card></div>
-            )
-        })
-       }
+            {!token && navigate("/")}
+            <div className='row'>
+                {
+                    moviesList.map((oneMovie, idx) => {
+                        return (
+                            <div className='col-3' key={idx}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} />
+                                    <Card.Body>
+                                        <Card.Title>{oneMovie.title}</Card.Title>
+                                        <Card.Text>{oneMovie.overview.substring(0, 100)}....</Card.Text>
+                                        <Button variant="primary">View Detail</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </>
+    );
 
-        </div>
-</>
-
-
-
-
-
-        
-    )
 }
-
 export default Listado
